19a46ccf305ca02b03f10003675a53fff16467f8
Van Rlan <vrlan@r2hstudent.org>
2022-04-25 13:44:50 -0400

    added footer social medias logo

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -3,25 +3,49 @@ import "../partials/footer.css";
 // import * as AiIcons from "react-icons/ai";
 // import * as FaIcons from "react-icons/fa";
 
-
 const Footer = () => {
   return (
     <>
       <footer className="footer">
-      <div className="container">
-            <div className="row">
-                <div className="footer-col-4">
-                    <h3>Follow Me</h3>
-                    <ul>
-                        <li>Facebook</li>
-                        <li>Instagram</li>
-                        <li>Instagram</li>
-                        <li>Youtube</li>
-                    </ul>
-                </div>
+        <div className="container">
+          <div className="row">
+            <div className="footer-col-4">
+              <h3>Follow Me</h3>
+              <ul>
+                <li>
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/van.rlan.986/"
+                >
+                <img src="/img/facebook-icon.png" alt="facebookIcon"/></a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://twitter.com/VanTheAnimeMan"
+                  >
+                  <img src="/img/twitter-icon.png" alt="twitterIcon" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/van-rlan/"
+                  >
+                  <img src="/img/linkedin-icon.webp" alt="linkedInLogo" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    href="https://www.instagram.com/vantheanimeman/"
+                  >
+                  <img src="/img/instagram-logo.png" alt="intagramLogo" />
+                  </a>
+                </li>
+              </ul>
             </div>
-            <hr/>
-            <p className="copyright">Copyright 2022 - Van Rlan</p>
+          </div>
+          <hr />
+          <p className="copyright">Copyright 2022 - Van Rlan</p>
         </div>
       </footer>
     </>
